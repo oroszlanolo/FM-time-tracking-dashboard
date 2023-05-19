b8aaa25d5664b33b80663d8b6bcad3ea8cab301e
oroszlanolo <balazsi.balint1111@gmail.com>
2023-05-20 00:57:37 +0200

    fixed gh pages deploy

--- src/app/tracking.service.ts
+++ src/app/tracking.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TrackCategory } from 'src/track';
 import { HttpClient } from '@angular/common/http';
-import { data } from 'src/data';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +11,7 @@ export class TrackingService {
   constructor(private http: HttpClient) { }
 
   getTracking() : Observable<TrackCategory[]> {
-    // const url = '/assets/data.json';
-    // return this.http.get<TrackCategory[]>(url);
-    return of(data);
+    const url = 'assets/data.json';
+    return this.http.get<TrackCategory[]>(url);
   }
 }
